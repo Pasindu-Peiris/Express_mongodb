3a4cb31a7434800cea507f82a643338aa841fa13
Pasindu Peiris <it21831454@my.sliit.lk>
2024-12-23 12:46:43 +0530

    Update userRoute.mjs

--- mongo_server/routes/userRoute.mjs
+++ mongo_server/routes/userRoute.mjs
@@ -4,11 +4,14 @@ import User from "../models/user.mjs";
 
 const userRouter = Router();
 
+//test user path working or not
 userRouter.get('/', (_, w) => {
     return w.status(200).json({ message: "User route is working" });
 })
 
+//save user into db
 userRouter.post('/save_user', async (c, w) => {
+
     const data = c.body;
     console.log(data);
 
@@ -26,6 +29,46 @@ userRouter.post('/save_user', async (c, w) => {
         return w.status(401).json({ message: error })
     }
 
+})
+
+//get all users in db
+userRouter.get('/get_user', async (_, w) => {
+
+    try {
+        const get_user = await User.find();
+
+        if (get_user) {
+            return w.status(200).json({ Users: get_user })
+        } else {
+            return w.status(200).json({ message: "Users not found" })
+        }
+
+    } catch (error) {
+        return w.status(400).json({ message: error });
+
+    }
+
+})
+
+//get user by id in db
+userRouter.get('/get-id/:id', async (c, w) => {
+
+    const { id } = c.params;
+    console.log(id)
+
+    try {
+
+        const user_id = await User.findOne({ _id: id })
+
+        if (user_id) {
+            return w.status(200).json({ user: user_id })
+        } else {
+            return w.status(401).json({ message: "user not found" })
+        }
+
+    } catch (error) {
+        return w.status(400).json({ message: error })
+    }
 
 })
 
