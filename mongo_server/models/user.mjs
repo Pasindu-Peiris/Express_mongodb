93bf838d6b66c74bd7bb90ddbff793cf82f01f02
Pasindu Peiris <it21831454@my.sliit.lk>
2024-12-23 12:51:05 +0530

    Update user.mjs

--- mongo_server/models/user.mjs
+++ mongo_server/models/user.mjs
@@ -1,19 +1,18 @@
 import { model, Schema } from "mongoose";
 
-const userModel = new Schema({
-
-    name: {
-        type: String,
-        required: true,
-        unique: true,
-
+const userModel = new Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        username: String,
+        email: String,
+        password: String,
     },
-    username: String,
-    email: String,
-    password: String,
-},
     { timestamps: true }
-)
+);
 
 const User = model("User", userModel);
 export default User;
