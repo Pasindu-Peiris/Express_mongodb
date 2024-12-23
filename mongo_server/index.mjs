f8d0d8a73bbd111ba06b938ebd341f6cb0a08e56
Pasindu Peiris <it21831454@my.sliit.lk>
2024-12-23 12:13:27 +0530

    Update index.mjs

--- mongo_server/index.mjs
+++ mongo_server/index.mjs
@@ -1,17 +1,30 @@
 import express from 'express';
 import 'dotenv/config';
+import dbconnect from './database/db.mjs';
+import rootRouter from './routes/rootRouter.mjs';
 
 const app = express();
 
 app.use(express.json());
 
+app.use('/api/v1', rootRouter); //connect routers
+
 // console.log(process.env.PORT);
 const port = process.env.PORT || 4005;
 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+dbconnect.then(() => {
+
+    app.listen(port, () => {
+        console.log(`Mongodb is connected on port ${port}`);
+    })
+
+    app.get('/', (_, res) => {
+        return res.status(200).json({ message: " mongo server is running" });
+    })
+
+    
+
+}).catch((err) => {
+    console.log("Error connecting to the database");
 })
 
-app.get('/', (_, res) => {
-    return res.status(200).json({ message: " mongo server is running" });
-})
