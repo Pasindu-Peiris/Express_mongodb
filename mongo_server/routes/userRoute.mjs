6f015cd4a8fdfce4bf85666767c96031adf7af20
Pasindu Peiris <it21831454@my.sliit.lk>
2024-12-23 18:22:35 +0530

    Update userRoute.mjs

--- mongo_server/routes/userRoute.mjs
+++ mongo_server/routes/userRoute.mjs
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import User from "../models/user.mjs";
+import Profile from "../models/profile.mjs";
 
 
 const userRouter = Router();
@@ -71,5 +72,47 @@ userRouter.get('/get-id/:id', async (c, w) => {
     }
 
 })
+
+//create profile 1:1 relationship and connect profile to user
+userRouter.post('/connect-profile/:userid', async (c, w) => {
+
+    const { userid } = c.params;
+    const { image } = c.body;
+
+    console.log(userid);
+
+    try {
+        const search_user = await User.findById(userid);
+
+        if (!search_user) {
+            return w.status(200).json({ message: "user not found!" });
+        }
+
+        const create_profile = await Profile.create({ user: userid, image });
+        const connected_user = await User.findByIdAndUpdate(userid, {
+            profile: create_profile._id,
+        },
+            {
+                new: true, // Return the updated document
+                runValidators: true, // Ensure validators are run
+            });
+
+        console.log(create_profile)
+        console.log(connected_user)
+
+        return w.status(200).json({ message: `${userid} user connected with profile id ${create_profile._id}`, user: connected_user, profile: create_profile })
+
+    } catch (error) {
+        return w.status(400).json({ message: error })
+    }
+
+
+
+})
+
+
+
+
+
 
 export default userRouter;
