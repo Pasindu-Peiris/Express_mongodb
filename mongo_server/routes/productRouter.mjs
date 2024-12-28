aaca6bb204f41c8bef36d9eef6a416fe85295409
Pasindu Peiris <it21831454@my.sliit.lk>
2024-12-28 12:50:45 +0530

    Update productRouter.mjs

--- mongo_server/routes/productRouter.mjs
+++ mongo_server/routes/productRouter.mjs
@@ -1,44 +1,52 @@
 import { Router } from "express";
 import Product from "../models/product.mjs";
+import User from "../models/user.mjs";
 
 const productRouter = Router();
 
 productRouter.get("/", async (_, w) => {
-    return w.status(200).json({ message: "product router is working !" });
+  return w.status(200).json({ message: "product router is working !" });
 });
 
 //get all products
 productRouter.get("/product-all", async (_, w) => {
-    try {
-        const allProduct = await Product.find();
+  try {
+    const allProduct = await Product.find();
 
-        if (!allProduct) {
-            return w.status(400).json({ message: "products not found!" });
-        }
-
-        return w.status(200).json({ products: allProduct });
-    } catch (error) {
-        console.log(error);
-        return w.status(500).json({ message: "internal server error!" });
+    if (!allProduct) {
+      return w.status(400).json({ message: "products not found!" });
     }
+
+    return w.status(200).json({ products: allProduct });
+  } catch (error) {
+    console.log(error);
+    return w.status(500).json({ message: "internal server error!" });
+  }
 });
 
 //create product [product create with user id [product cerate => add product _id into User document product array[]]]
 productRouter.post("/product-create", async (c, w) => {
+  const { user, title, image } = c.body;
 
-    const data = c.body
+  try {
+    const created_product = await Product.create({ user, title, image }); //create product
 
-    console.log(data);
+    const update_user_product = await User.updateOne(
+      //update user products array using created new product
+      { _id: user },
+      { $push: { products: created_product._id } },
+      {
+        new: true, // Return the updated profile
+        runValidators: true, // Validate the fields before saving
+      }
+    );
 
-    try {
+    return w.status(200).json({message : "product created with user"})
 
-        const created_product = await Product.create(data);
-
-    } catch (error) {
-        console.log(error);
-        return w.status(500).json({ message: "internal server error!" });
-    }
-
+  } catch (error) {
+    console.log(error);
+    return w.status(500).json({ message: "internal server error!" });
+  }
 });
 
 export default productRouter;
