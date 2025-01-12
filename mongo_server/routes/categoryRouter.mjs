2a868902e0d81f747ccb29780cc3db8f47c83da0
Pasindu Peiris <it21831454@my.sliit.lk>
2025-01-12 20:55:02 +0530

    Create categoryRouter.mjs

---
+++ mongo_server/routes/categoryRouter.mjs
@@ -0,0 +1,12 @@
+import { Router } from "express";
+
+const categoryRouter = Router();
+
+categoryRouter.get('/', async (_, w) => {
+    return w.status(200).json({message : "category router is working !"});
+})
+
+//create category
+
+
+export default categoryRouter;
