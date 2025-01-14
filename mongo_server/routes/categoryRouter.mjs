775a98fe09fced48869757aee334c867e6596d89
Pasindu Peiris <it21831454@my.sliit.lk>
2025-01-14 12:50:07 +0530

    Update categoryRouter.mjs

--- mongo_server/routes/categoryRouter.mjs
+++ mongo_server/routes/categoryRouter.mjs
@@ -3,34 +3,29 @@ import Category from "../models/category.mjs";
 
 const categoryRouter = Router();
 
-categoryRouter.get('/', async (_, w) => {
-    return w.status(200).json({message : "category router is working !"});
-})
-
-//create category
-
+categoryRouter.get("/", async (_, w) => {
+    return w.status(200).json({ message: "category router is working !" });
+});
 
 //get category
-categoryRouter.get('/all-category', async (c, w) => {
-
+categoryRouter.get("/all-category", async (c, w) => {
     try {
-
         const all_category = await Category.find();
 
-        if(!all_category){
+        if (!all_category) {
             return w.status(400).json({ message: "category not found!" });
         }
 
-        return w.status(200).json({ Category: all_category});
-        
+        return w.status(200).json({ Category: all_category });
     } catch (error) {
         console.log(error);
         return w.status(500).json({ message: "internal server error!" });
     }
+});
 
+//create category
+categoryRouter.post("/create-category", async (c, w) => {
+    
 })
-
-
-
 
 export default categoryRouter;
