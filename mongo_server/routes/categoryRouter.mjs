6855f5b29cb25d8cac51a64048dca69b4d4261d2
Pasindu Peiris <it21831454@my.sliit.lk>
2025-01-14 21:58:01 +0530

    Update categoryRouter.mjs

--- mongo_server/routes/categoryRouter.mjs
+++ mongo_server/routes/categoryRouter.mjs
@@ -52,26 +52,33 @@ categoryRouter.put('/connect-product-category/:cate_id', async (c, w) => {
     const category_id = c.params.cate_id;
     const product_id = c.query.prodid;
 
+    if (!category_id || !product_id) {
+        return w.status(400).json({ message: "must provide category_id and product_id both !" })
+    }
+
     try {
 
-        const product_found = await Product.findById(category_id);
-        const category_found = await Category.findById(product_id);
+        const product_found = await Product.findById(product_id)
+        const category_found = await Category.findById(category_id);
+
+        if (!product_found || !category_id) {
+            return w.status(400).json({ message: "product or category not found !" });
+        }
 
         product_found.category.push(category_id);
-        category_id.product.push(product_id)
+        category_found.product.push(product_id);
 
         await product_found.save();
         await category_found.save();
 
-        return w.stats(200).json({message : "category connected with product !"})
-
+        return w.status(200).json({ message: "category connected with product !" })
 
     } catch (error) {
         console.log(error);
         return w.status(500).json({ message: "internal server error!" });
     }
 
-    console.log(product_id, category_id)
+
 })
 
 export default categoryRouter;
